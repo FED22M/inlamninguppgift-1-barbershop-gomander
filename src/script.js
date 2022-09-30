217a4f07b97f5fb62404b2d6e3a22f2ed60d82c1
Gunnar Omander <gunnar.omander@gmail.com>
2022-09-30 10:52:58 +0200

    Style header and services for mobile

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function toggleMenu() {
-  const nav = document.getElementById('nav-list');
+  const nav = document.getElementById('nav');
   console.log(nav.style.display);
   if (nav.style.display === 'block') nav.style.display = 'none';
   else nav.style.display = 'block';
